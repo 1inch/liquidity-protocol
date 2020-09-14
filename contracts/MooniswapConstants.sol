177292985af69663f7a65118a834de46e710b91e
Mikhail Melnik <by.zumzoom@gmail.com>
2020-09-14 17:46:16 +0300

    raise max fee limit

--- contracts/MooniswapConstants.sol
+++ contracts/MooniswapConstants.sol
@@ -3,6 +3,6 @@
 pragma solidity ^0.6.0;
 
 contract MooniswapConstants {
-    uint256 public constant MAX_FEE = 0.003e18; // 0.3%
+    uint256 public constant MAX_FEE = 0.05e18; // 5%
     uint256 public constant MAX_DECAY_PERIOD = 1 days;
 }
