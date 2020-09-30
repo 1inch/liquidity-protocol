b264cfa3f000500a4c0fc3608a1397f81add1e71
Mikhail Melnik <by.zumzoom@gmail.com>
2020-09-30 12:27:38 +0300

    simplify deposit

--- contracts/Mooniswap.sol
+++ contracts/Mooniswap.sol
@@ -126,45 +126,47 @@ contract Mooniswap is ERC20, ReentrancyGuard, Ownable, MooniswapConstants {
         IERC20[2] memory _tokens = [token0, token1];
         require(msg.value == (_tokens[0].isETH() ? maxAmounts[0] : (_tokens[1].isETH() ? maxAmounts[1] : 0)), "Mooniswap: wrong value usage");
 
-        uint256[2] memory realBalances;
-        for (uint i = 0; i < realBalances.length; i++) {
-            realBalances[i] = _tokens[i].uniBalanceOf(address(this)).sub(_tokens[i].isETH() ? msg.value : 0);
-        }
-
         uint256 totalSupply = totalSupply();
+
         if (totalSupply == 0) {
             fairSupply = _BASE_SUPPLY.mul(99);
             _mint(address(this), _BASE_SUPPLY); // Donate up to 1%
-
-            // Use the greatest token amount but not less than 99k for the initial supply
+            
             for (uint i = 0; i < maxAmounts.length; i++) {
                 fairSupply = Math.max(fairSupply, maxAmounts[i]);
+
+                uint256 amount = maxAmounts[i];
+                require(amount > 0, "Mooniswap: amount is zero");
+                require(amount >= minAmounts[i], "Mooniswap: minAmount not reached");
+
+                _tokens[i].uniTransferFrom(msg.sender, address(this), amount);
             }
         }
         else {
+            uint256[2] memory realBalances;
+            for (uint i = 0; i < realBalances.length; i++) {
+                realBalances[i] = _tokens[i].uniBalanceOf(address(this)).sub(_tokens[i].isETH() ? msg.value : 0);
+            }
+
             // Pre-compute fair supply
             fairSupply = type(uint256).max;
             for (uint i = 0; i < maxAmounts.length; i++) {
                 fairSupply = Math.min(fairSupply, totalSupply.mul(maxAmounts[i]).div(realBalances[i]));
             }
-        }
 
-        uint256 fairSupplyCached = fairSupply;
-        for (uint i = 0; i < maxAmounts.length; i++) {
-            require(maxAmounts[i] > 0, "Mooniswap: amount is zero");
-            uint256 amount = (totalSupply == 0) ? maxAmounts[i] :
-                realBalances[i].mul(fairSupplyCached).add(totalSupply - 1).div(totalSupply);
-            require(amount >= minAmounts[i], "Mooniswap: minAmount not reached");
+            uint256 fairSupplyCached = fairSupply;
 
-            _tokens[i].uniTransferFrom(msg.sender, address(this), amount);
-            if (totalSupply > 0) {
+            for (uint i = 0; i < maxAmounts.length; i++) {
+                require(maxAmounts[i] > 0, "Mooniswap: amount is zero");
+                uint256 amount = realBalances[i].mul(fairSupplyCached).add(totalSupply - 1).div(totalSupply);
+                require(amount >= minAmounts[i], "Mooniswap: minAmount not reached");
+
+                _tokens[i].uniTransferFrom(msg.sender, address(this), amount);
                 uint256 confirmed = _tokens[i].uniBalanceOf(address(this)).sub(realBalances[i]);
                 fairSupply = Math.min(fairSupply, totalSupply.mul(confirmed).div(realBalances[i]));
             }
-        }
 
-        uint256 _decayPeriod = decayPeriod;  // gas savings
-        if (totalSupply > 0) {
+            uint256 _decayPeriod = decayPeriod;  // gas savings
             for (uint i = 0; i < maxAmounts.length; i++) {
                 virtualBalancesForRemoval[_tokens[i]].scale(_decayPeriod, realBalances[i], totalSupply.add(fairSupply), totalSupply);
                 virtualBalancesForAddition[_tokens[i]].scale(_decayPeriod, realBalances[i], totalSupply.add(fairSupply), totalSupply);
