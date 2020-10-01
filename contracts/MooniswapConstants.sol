30ef889ce0a62e1d383c142234ea956bdb0a7ae7
Mikhail Melnik <by.zumzoom@gmail.com>
2020-10-01 18:03:39 +0300

    lower MAX_DECAY_PERIOD

--- contracts/MooniswapConstants.sol
+++ contracts/MooniswapConstants.sol
@@ -5,6 +5,6 @@ pragma solidity ^0.6.0;
 contract MooniswapConstants {
     uint256 internal constant _MAX_FEE = 0.1e18; // 10%
     uint256 internal constant _MAX_SHARE = 0.5e18; // 50%
-    uint256 internal constant _MAX_DECAY_PERIOD = 1 days;
+    uint256 internal constant _MAX_DECAY_PERIOD = 1 hours;
     uint256 internal constant _FEE_DENOMINATOR = 1e18;
 }
