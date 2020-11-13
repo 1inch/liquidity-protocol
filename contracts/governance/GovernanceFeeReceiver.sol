9fb22fca3b35516b928422bc39f96d0f297495fd
Mikhail Melnik <by.zumzoom@gmail.com>
2020-11-13 14:34:55 +0300

    fix GovernanceFeeReceiver

--- contracts/governance/GovernanceFeeReceiver.sol
+++ contracts/governance/GovernanceFeeReceiver.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "../Mooniswap.sol";
 import "../libraries/UniERC20.sol";
 import "../libraries/VirtualBalance.sol";
+import "./rewards/IRewardDistributionRecipient.sol";
+import "../Mooniswap.sol";
 
 
 contract GovernanceFeeReceiver {
     using SafeMath for uint256;
+    using SafeERC20 for IERC20;
     using UniERC20 for IERC20;
     using VirtualBalance for VirtualBalance.Data;
 
@@ -18,43 +20,52 @@ contract GovernanceFeeReceiver {
     uint256 private constant _SPREAD_FEE_MULTIPLIER = 10;
     uint256 private constant _MAX_LIQUIDITY_SHARE = 100;
 
-    IERC20 public immutable moonToken;
-    IMooniswapFactoryGovernance public immutable mooniswapFactoryGovernance;
+    IERC20 public immutable targetToken;
+    IRewardDistributionRecipient public immutable rewards;
 
-    constructor(IERC20 _moonToken, IMooniswapFactoryGovernance _mooniswapFactoryGovernance) public {
-        moonToken = _moonToken;
-        mooniswapFactoryGovernance = _mooniswapFactoryGovernance;
+    receive() external payable {}  // solhint-disable-line no-empty-blocks
+
+    constructor(IERC20 _targetToken, IRewardDistributionRecipient _rewards) public {
+        targetToken = _targetToken;
+        rewards = _rewards;
     }
 
     modifier validSpread(Mooniswap mooniswap) {
         _validateSpread(mooniswap);
+
         _;
     }
 
-    function unwrapLPTokens(Mooniswap mooniswap, uint256 amount, uint256[] memory minReturns) external validSpread(mooniswap) {
-        mooniswap.withdraw(amount, minReturns);
+    function unwrapLPTokens(Mooniswap mooniswap) external validSpread(mooniswap) {
+        mooniswap.withdraw(mooniswap.balanceOf(address(this)), new uint256[](0));
     }
 
     function swap(IERC20[] memory path) external {
-        require(path[path.length - 1] == moonToken, "should swap to moon token");
+        require(path[path.length - 1] == targetToken, "Should swap to target token");
 
-        uint256 amount = path[0].balanceOf(address(this));
+        uint256 amount = path[0].uniBalanceOf(address(this));
         uint256 swapAmount = amount;
         for (uint256 i = 1; i < path.length; i += 2) {
             Mooniswap mooniswap = Mooniswap(address(path[i]));
             _validateSpread(mooniswap);
-            uint256 maxCurSwapAmount = path[i-1].balanceOf(address(mooniswap)).div(_MAX_LIQUIDITY_SHARE);
+            uint256 maxCurSwapAmount = path[i-1].uniBalanceOf(address(mooniswap)).div(_MAX_LIQUIDITY_SHARE);
             if (maxCurSwapAmount < swapAmount) {
                 amount = amount.mul(maxCurSwapAmount).div(swapAmount);
                 swapAmount = maxCurSwapAmount;
             }
             swapAmount = mooniswap.getReturn(path[i-1], path[i+1], swapAmount);
         }
-        for (uint256 i = 1; i < path.length - 2; i += 2) {
-            amount = Mooniswap(address(path[i])).swap(path[i-1], path[i+1], amount, 0, address(this));
+        for (uint256 i = 1; i < path.length; i += 2) {
+            if (path[i-1].isETH()) {
+                amount = Mooniswap(address(path[i])).swap{value: amount}(path[i-1], path[i+1], amount, 0, address(this));
+            } else {
+                path[i-1].safeApprove(address(path[i]), amount);
+                amount = Mooniswap(address(path[i])).swap(path[i-1], path[i+1], amount, 0, address(this));
+            }
         }
 
-        moonToken.transfer(mooniswapFactoryGovernance.governanceFeeReceiver(), amount);
+        targetToken.transfer(address(rewards), amount);
+        rewards.notifyRewardAmount(amount);
     }
 
     function _validateSpread(Mooniswap mooniswap) private view {
