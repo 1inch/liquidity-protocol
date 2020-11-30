41d8030f1acc0d37f15d305272bb1de85c98ef06
Anton Bukov <k06aaa@gmail.com>
2020-11-30 15:06:18 +0300

    Fixes

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,6 +2,8 @@ const Migrations = artifacts.require('./Migrations.sol');
 const GovernanceMothership = artifacts.require('./inch/GovernanceMothership.sol');
 const MooniswapDeployer = artifacts.require('./MooniswapDeployer.sol');
 const MooniswapFactory = artifacts.require('./MooniswapFactory.sol');
+const ReferralFeeReceiver = artifacts.require('./ReferralFeeReceiver.sol');
+const GovernanceFeeReceiver = artifacts.require('./governance/GovernanceFeeReceiver.sol');
 const Rewards = artifacts.require('./governance/rewards/Rewards.sol');
 const TokenMock = artifacts.require('./mocks/TokenMock.sol');
 
@@ -11,28 +13,38 @@ const POOL_OWNER = {
 };
 
 module.exports = function(deployer, network) {
-    let token, governanceMothership, mooniswapDeployer, mooniswapFactory, poolOwner;
-    deployer.deploy(Migrations);
+    deployer.then(async () => {
+        await deployer.deploy(Migrations);
 
-    deployer.deploy(TokenMock, 'BOOM', 'BOOM', 18)
-        .then((t) => {
-            token = t;
-            deployer.deploy(GovernanceMothership, token.address)
-                .then((gm) => {
-                    governanceMothership = gm;
-                    deployer.deploy(MooniswapDeployer).then((md) => {
-                        mooniswapDeployer = md;
-                        deployer.deploy(
-                            MooniswapFactory,
-                            POOL_OWNER[network],
-                            mooniswapDeployer.address,
-                            governanceMothership.address
-                        )
-                            .then((mf) => {
-                                mooniswapFactory = mf;
-                                deployer.deploy(Rewards);
-                            });
-                    });
-                });
-        });
+        const token = await deployer.deploy(TokenMock, 'BOOM', 'BOOM', 18);
+        const governanceMothership = await deployer.deploy(GovernanceMothership, token.address);
+
+        // Mooniswap Factory
+
+        const mooniswapDeployer = await deployer.deploy(MooniswapDeployer);
+        const mooniswapFactory = await deployer.deploy(
+            MooniswapFactory,
+            POOL_OWNER[network],
+            mooniswapDeployer.address,
+            governanceMothership.address
+        );
+        await governanceMothership.addModule(mooniswapFactory.address);
+
+        // Governance
+
+        const rewards = await deployer.deploy(Rewards);
+        await governanceMothership.addModule(rewards.address);
+
+        const governanceFeeReceiver = await deployer.deploy(GovernanceFeeReceiver, token.address, rewards.address);
+        await governanceMothership.setGovernanceFeeReceiver(governanceFeeReceiver.address);
+        await rewards.setRewardDistribution(governanceFeeReceiver.address);
+        await rewards.transferOwneship(POOL_OWNER[network]);
+
+        const referralFeeReceiver = deployer.deploy(ReferralFeeReceiver, token.address);
+        await governanceMothership.setReferralFeeReceiver(referralFeeReceiver);
+
+        // Latest
+
+        await governanceMothership.transferOwneship(POOL_OWNER[network]);
+    });
 };
