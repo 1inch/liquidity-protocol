2748e1b8581bbd402c367ad35f7db787808546b8
deacix <info@deacix.de>
2020-11-30 12:06:44 +0100

    start implementing

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,9 +1,38 @@
 const Migrations = artifacts.require('./Migrations.sol');
-// const MooniFactory = artifacts.require('./MooniFactory.sol');
-// const Mooniswap = artifacts.require('./Mooniswap.sol');
+const GovernanceMothership = artifacts.require('./inch/GovernanceMothership.sol');
+const MooniswapDeployer = artifacts.require('./MooniswapDeployer.sol');
+const MooniswapFactory = artifacts.require('./MooniswapFactory.sol');
+const Rewards = artifacts.require('./governance/rewards/Rewards.sol');
+const TokenMock = artifacts.require('./mocks/TokenMock.sol');
 
-module.exports = function (deployer) {
+const POOL_OWNER = {
+    'kovan': '0x1cB37a0606003654b302bbD8fea408BFa066c6Ef',
+    'development': '0x1cB37a0606003654b302bbD8fea408BFa066c6Ef'
+};
+
+module.exports = function(deployer, network) {
+    let token, governanceMothership, mooniswapDeployer, mooniswapFactory, poolOwner;
     deployer.deploy(Migrations);
-    // deployer.deploy(MooniFactory);
-    // deployer.deploy(Mooniswap);
+
+    deployer.deploy(TokenMock, 'BOOM', 'BOOM', 18)
+        .then((t) => {
+            token = t;
+            deployer.deploy(GovernanceMothership, token.address)
+                .then((gm) => {
+                    governanceMothership = gm;
+                    deployer.deploy(MooniswapDeployer).then((md) => {
+                        mooniswapDeployer = md;
+                        deployer.deploy(
+                            MooniswapFactory,
+                            POOL_OWNER[network],
+                            mooniswapDeployer.address,
+                            governanceMothership.address
+                        )
+                            .then((mf) => {
+                                mooniswapFactory = mf;
+                                deployer.deploy(Rewards);
+                            });
+                    });
+                });
+        });
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,42 @@
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const kovanPrivateKey = '3ddc9f73d55425ca6ce31a26c2e4a70743d85f567436ea5028878ff80083b8b0';
+const endpointUrl = 'https://web3-node.1inch.exchange';
+const kovanEndpointUrl = 'https://kovan.infura.io/v3/9e4cfe82e43c48e2afc547c40145dab8';
+
 module.exports = {
+    networks: {
+        development: {
+            host: '127.0.0.1',
+            port: 9545,
+            network_id: '5777'
+        },
+        kovan: {
+            provider: function() {
+                return new HDWalletProvider(
+                    [kovanPrivateKey],
+                    kovanEndpointUrl
+                );
+            },
+            gas: 5000000,
+            gasPrice: 25000000000,
+            network_id: 42
+        }
+    },
     compilers: {
         solc: {
             version: '0.6.12',
             settings: {
                 optimizer: {
                     enabled: true,
-                    runs: 10000,
+                    runs: 10000
                 }
             }
-        },
+        }
     },
-    plugins: ["solidity-coverage"],
+    plugins: ['solidity-coverage'],
     mocha: { // https://github.com/cgewecke/eth-gas-reporter
         reporter: 'eth-gas-reporter',
-        reporterOptions : {
+        reporterOptions: {
             currency: 'USD',
             gasPrice: 10,
             onlyCalledMethods: true,
