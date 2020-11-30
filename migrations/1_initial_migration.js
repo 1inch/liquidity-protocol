d4fe80592cb13b58a4746b881048bce3a818182c
Anton Bukov <k06aaa@gmail.com>
2020-11-30 16:50:25 +0300

    Fixed

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -9,6 +9,7 @@ const TokenMock = artifacts.require('./mocks/TokenMock.sol');
 
 const POOL_OWNER = {
     'kovan': '0x1cB37a0606003654b302bbD8fea408BFa066c6Ef',
+    'kovan-fork': '0x1cB37a0606003654b302bbD8fea408BFa066c6Ef',
     'development': '0x1cB37a0606003654b302bbD8fea408BFa066c6Ef'
 };
 
@@ -32,19 +33,19 @@ module.exports = function(deployer, network) {
 
         // Governance
 
-        const rewards = await deployer.deploy(Rewards);
+        const rewards = await deployer.deploy(Rewards, token.address, governanceMothership.address);
         await governanceMothership.addModule(rewards.address);
 
         const governanceFeeReceiver = await deployer.deploy(GovernanceFeeReceiver, token.address, rewards.address);
-        await governanceMothership.setGovernanceFeeReceiver(governanceFeeReceiver.address);
+        await mooniswapFactory.setGovernanceFeeReceiver(governanceFeeReceiver.address);
         await rewards.setRewardDistribution(governanceFeeReceiver.address);
-        await rewards.transferOwneship(POOL_OWNER[network]);
 
-        const referralFeeReceiver = deployer.deploy(ReferralFeeReceiver, token.address);
-        await governanceMothership.setReferralFeeReceiver(referralFeeReceiver);
+        const referralFeeReceiver = await deployer.deploy(ReferralFeeReceiver, token.address);
+        await mooniswapFactory.setReferralFeeReceiver(referralFeeReceiver.address);
 
-        // Latest
+        // Transfer Ownership
 
-        await governanceMothership.transferOwneship(POOL_OWNER[network]);
+        await governanceMothership.transferOwnership(POOL_OWNER[network]);
+        await rewards.transferOwnership(POOL_OWNER[network]);
     });
 };
