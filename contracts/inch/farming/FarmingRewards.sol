3c778388ce853a4d746cd63ec95f992ec19738bf
Mikhail Melnik <by.zumzoom@gmail.com>
2020-12-15 22:17:37 +0300

    extract rewards to base class

--- contracts/inch/farming/FarmingRewards.sol
+++ contracts/inch/farming/FarmingRewards.sol
@@ -1,46 +1,31 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.12;
 
-import "@openzeppelin/contracts/math/Math.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
 import "../../Mooniswap.sol";
-import "../../utils/BalanceAccounting.sol";
-import "../../utils/RewardDistributionRecipient.sol";
-import "./FarmingVoter.sol";
+import "../../libraries/Voting.sol";
+import "../../utils/BaseRewards.sol";
 
 
-contract FarmingRewards is RewardDistributionRecipient, FarmingVoter {
-    using SafeMath for uint256;
+contract FarmingRewards is MooniswapConstants, BaseRewards {
+    using Vote for Vote.Data;
+    using Voting for Voting.Data;
 
-    event RewardAdded(uint256 reward);
-    event RewardPaid(address indexed user, uint256 reward);
     event Staked(address indexed user, uint256 amount);
     event Withdrawn(address indexed user, uint256 amount);
+    event FeeVoteUpdate(address indexed user, uint256 fee, bool isDefault, uint256 amount);
+    event SlippageFeeVoteUpdate(address indexed user, uint256 slippageFee, bool isDefault, uint256 amount);
+    event DecayPeriodVoteUpdate(address indexed user, uint256 decayPeriod, bool isDefault, uint256 amount);
 
-    uint256 public constant DURATION = 7 days;
-    IERC20 public immutable gift;
+    Mooniswap public immutable mooniswap;
+    IMooniswapFactoryGovernance public immutable mooniswapFactoryGovernance;
+    Voting.Data private _fee;
+    Voting.Data private _slippageFee;
+    Voting.Data private _decayPeriod;
 
-    uint256 public periodFinish = 0;
-    uint256 public rewardRate = 0;
-    uint256 public lastUpdateTime;
-    uint256 public rewardPerTokenStored;
-    mapping(address => uint256) public userRewardPerTokenPaid;
-    mapping(address => uint256) public rewards;
-
-    modifier updateReward(address account) {
-        rewardPerTokenStored = rewardPerToken();
-        lastUpdateTime = lastTimeRewardApplicable();
-        if (account != address(0)) {
-            rewards[account] = earned(account);
-            userRewardPerTokenPaid[account] = rewardPerTokenStored;
-        }
-
-        _;
-    }
-
-    constructor(Mooniswap _mooniswap, IERC20 _gift) public FarmingVoter(_mooniswap) {
-        gift = _gift;
+    constructor(Mooniswap _mooniswap, IERC20 _gift) public BaseRewards(_gift) {
+        mooniswap = _mooniswap;
+        mooniswapFactoryGovernance = _mooniswap.mooniswapFactoryGovernance();
     }
 
     function stake(uint256 amount) public updateReward(msg.sender) {
@@ -57,62 +42,112 @@ contract FarmingRewards is RewardDistributionRecipient, FarmingVoter {
         emit Withdrawn(msg.sender, amount);
     }
 
-
-    function lastTimeRewardApplicable() public view returns (uint256) {
-        return Math.min(block.timestamp, periodFinish);
-    }
-
-    function rewardPerToken() public view returns (uint256) {
-        if (totalSupply() == 0) {
-            return rewardPerTokenStored;
-        }
-        return
-            rewardPerTokenStored.add(
-                lastTimeRewardApplicable()
-                    .sub(lastUpdateTime)
-                    .mul(rewardRate)
-                    .mul(1e18)
-                    .div(totalSupply())
-            );
-    }
-
-    function earned(address account) public view returns (uint256) {
-        return
-            balanceOf(account)
-                .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
-                .div(1e18)
-                .add(rewards[account]);
-    }
-
     function exit() external {
         withdraw(balanceOf(msg.sender));
         getReward();
     }
 
-    function getReward() public updateReward(msg.sender) {
-        uint256 reward = earned(msg.sender);
-        if (reward > 0) {
-            rewards[msg.sender] = 0;
-            gift.transfer(msg.sender, reward);
-            emit RewardPaid(msg.sender, reward);
+    function fee() public view returns(uint256) {
+        return _fee.result;
+    }
+
+    function slippageFee() public view returns(uint256) {
+        return _slippageFee.result;
+    }
+
+    function decayPeriod() public view returns(uint256) {
+        return _decayPeriod.result;
+    }
+
+    function feeVotes(address user) external view returns(uint256) {
+        return _fee.votes[user].get(mooniswapFactoryGovernance.defaultFee);
+    }
+
+    function slippageFeeVotes(address user) external view returns(uint256) {
+        return _slippageFee.votes[user].get(mooniswapFactoryGovernance.defaultSlippageFee);
+    }
+
+    function decayPeriodVotes(address user) external view returns(uint256) {
+        return _decayPeriod.votes[user].get(mooniswapFactoryGovernance.defaultDecayPeriod);
+    }
+
+    function feeVote(uint256 vote) external {
+        require(vote <= _MAX_FEE, "Fee vote is too high");
+
+        _fee.updateVote(msg.sender, _fee.votes[msg.sender], Vote.init(vote), balanceOf(msg.sender), totalSupply(), mooniswapFactoryGovernance.defaultFee(), _emitFeeVoteUpdate);
+        mooniswap.feeVote(_fee.result);
+    }
+
+    function slippageFeeVote(uint256 vote) external {
+        require(vote <= _MAX_SLIPPAGE_FEE, "Slippage fee vote is too high");
+
+        _slippageFee.updateVote(msg.sender, _slippageFee.votes[msg.sender], Vote.init(vote), balanceOf(msg.sender), totalSupply(), mooniswapFactoryGovernance.defaultSlippageFee(), _emitSlippageFeeVoteUpdate);
+        mooniswap.slippageFeeVote(_slippageFee.result);
+    }
+
+    function decayPeriodVote(uint256 vote) external {
+        require(vote <= _MAX_DECAY_PERIOD, "Decay period vote is too high");
+        require(vote >= _MIN_DECAY_PERIOD, "Decay period vote is too low");
+
+        _decayPeriod.updateVote(msg.sender, _decayPeriod.votes[msg.sender], Vote.init(vote), balanceOf(msg.sender), totalSupply(), mooniswapFactoryGovernance.defaultDecayPeriod(), _emitDecayPeriodVoteUpdate);
+        mooniswap.decayPeriodVote(_decayPeriod.result);
+    }
+
+    function discardFeeVote() external {
+        _fee.updateVote(msg.sender, _fee.votes[msg.sender], Vote.init(), balanceOf(msg.sender), totalSupply(), mooniswapFactoryGovernance.defaultFee(), _emitFeeVoteUpdate);
+        _vote(_fee, mooniswap.feeVote, mooniswap.discardFeeVote);
+    }
+
+    function discardSlippageFeeVote() external {
+        _slippageFee.updateVote(msg.sender, _slippageFee.votes[msg.sender], Vote.init(), balanceOf(msg.sender), totalSupply(), mooniswapFactoryGovernance.defaultSlippageFee(), _emitSlippageFeeVoteUpdate);
+        _vote(_slippageFee, mooniswap.slippageFeeVote, mooniswap.discardSlippageFeeVote);
+    }
+
+    function discardDecayPeriodVote() external {
+        _decayPeriod.updateVote(msg.sender, _decayPeriod.votes[msg.sender], Vote.init(), balanceOf(msg.sender), totalSupply(), mooniswapFactoryGovernance.defaultDecayPeriod(), _emitDecayPeriodVoteUpdate);
+        _vote(_decayPeriod, mooniswap.decayPeriodVote, mooniswap.discardDecayPeriodVote);
+    }
+
+    function _mint(address account, uint256 amount) internal override {
+        super._mint(account, amount);
+
+        uint256 newBalance = balanceOf(account);
+        _updateVotes(account, newBalance.sub(amount), newBalance, totalSupply());
+    }
+
+    function _burn(address account, uint256 amount) internal override {
+        super._burn(account, amount);
+
+        uint256 newBalance = balanceOf(account);
+        _updateVotes(account, newBalance.add(amount), newBalance, totalSupply());
+    }
+
+    function _updateVotes(address account, uint256 balance, uint256 newBalance, uint256 newTotalSupply) private {
+        _fee.updateBalance(account, _fee.votes[account], balance, newBalance, newTotalSupply, _DEFAULT_FEE, _emitFeeVoteUpdate);
+        _vote(_fee, mooniswap.feeVote, mooniswap.discardFeeVote);
+        _slippageFee.updateBalance(account, _slippageFee.votes[account], balance, newBalance, newTotalSupply, _DEFAULT_SLIPPAGE_FEE, _emitSlippageFeeVoteUpdate);
+        _vote(_slippageFee, mooniswap.slippageFeeVote, mooniswap.discardSlippageFeeVote);
+        _decayPeriod.updateBalance(account, _decayPeriod.votes[account], balance, newBalance, newTotalSupply, _DEFAULT_DECAY_PERIOD, _emitDecayPeriodVoteUpdate);
+        _vote(_decayPeriod, mooniswap.decayPeriodVote, mooniswap.discardDecayPeriodVote);
+    }
+
+    function _vote(Voting.Data storage votingData, function(uint256) external vote, function() external discardVote) private {
+        if (votingData._weightedSum == 0) {
+            discardVote();
+        } else {
+            vote(votingData.result);
         }
     }
 
-    function notifyRewardAmount(uint256 reward)
-        external
-        override
-        onlyRewardDistribution
-        updateReward(address(0))
-    {
-        if (block.timestamp >= periodFinish) {
-            rewardRate = reward.div(DURATION);
-        } else {
-            uint256 remaining = periodFinish.sub(block.timestamp);
-            uint256 leftover = remaining.mul(rewardRate);
-            rewardRate = reward.add(leftover).div(DURATION);
-        }
-        lastUpdateTime = block.timestamp;
-        periodFinish = block.timestamp.add(DURATION);
-        emit RewardAdded(reward);
+    function _emitFeeVoteUpdate(address account, uint256 newFee, bool isDefault, uint256 newBalance) private {
+        emit FeeVoteUpdate(account, newFee, isDefault, newBalance);
+    }
+
+    function _emitSlippageFeeVoteUpdate(address account, uint256 newSlippageFee, bool isDefault, uint256 newBalance) private {
+        emit SlippageFeeVoteUpdate(account, newSlippageFee, isDefault, newBalance);
+    }
+
+    function _emitDecayPeriodVoteUpdate(address account, uint256 newDecayPeriod, bool isDefault, uint256 newBalance) private {
+        emit DecayPeriodVoteUpdate(account, newDecayPeriod, isDefault, newBalance);
     }
 }
---
+++ contracts/utils/BaseRewards.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/Math.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./BalanceAccounting.sol";
+
+
+contract BaseRewards is Ownable, BalanceAccounting {
+    event RewardAdded(uint256 reward);
+    event RewardPaid(address indexed user, uint256 reward);
+
+    uint256 public constant DURATION = 7 days;
+
+    address public rewardDistribution;
+    IERC20 public immutable gift;
+    uint256 public periodFinish = 0;
+    uint256 public rewardRate = 0;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+
+        _;
+    }
+
+    modifier onlyRewardDistribution() {
+        require(_msgSender() == rewardDistribution, "Access denied");
+        _;
+    }
+
+    constructor(IERC20 _gift) public {
+        gift = _gift;
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return Math.min(block.timestamp, periodFinish);
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (totalSupply() == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+            rewardPerTokenStored.add(
+                lastTimeRewardApplicable()
+                    .sub(lastUpdateTime)
+                    .mul(rewardRate)
+                    .mul(1e18)
+                    .div(totalSupply())
+            );
+    }
+
+    function earned(address account) public view returns (uint256) {
+        return
+            balanceOf(account)
+                .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
+                .div(1e18)
+                .add(rewards[account]);
+    }
+
+    function getReward() public updateReward(msg.sender) {
+        uint256 reward = earned(msg.sender);
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            gift.transfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    function notifyRewardAmount(uint256 reward) external onlyRewardDistribution updateReward(address(0)) {
+        if (block.timestamp >= periodFinish) {
+            rewardRate = reward.div(DURATION);
+        } else {
+            uint256 remaining = periodFinish.sub(block.timestamp);
+            uint256 leftover = remaining.mul(rewardRate);
+            rewardRate = reward.add(leftover).div(DURATION);
+        }
+        lastUpdateTime = block.timestamp;
+        periodFinish = block.timestamp.add(DURATION);
+        emit RewardAdded(reward);
+    }
+
+    function setRewardDistribution(address _rewardDistribution) external onlyOwner {
+        rewardDistribution = _rewardDistribution;
+    }
+}
