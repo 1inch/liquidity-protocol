0d563ffe87138055236c9f661815efcaf01caaa6
Mikhail Melnik <by.zumzoom@gmail.com>
2020-12-17 12:39:52 +0300

    add farming migration

---
+++ migrations/2_pools_migration.js
@@ -0,0 +1,4 @@
+module.exports = function (deployer, network) {
+    return deployer.then(async () => {
+    });
+};
---
+++ migrations/3_farming_migration.js
@@ -0,0 +1,47 @@
+const { ether } = require('@openzeppelin/test-helpers');
+
+const TokenMock = artifacts.require('./mocks/TokenMock.sol');
+const FarmingRewards = artifacts.require('./inch/farming/FarmingRewards.sol');
+
+const TOKEN = {
+    kovan: '0x9F6A694123e5599a07f984eb8c0F3A475F553A03',
+    'kovan-fork': '0x9F6A694123e5599a07f984eb8c0F3A475F553A03',
+    mainnet: '0x28ed0b47EeE1F467D182620a333Fe69415Ba9AC9',
+    'mainnet-fork': '0x28ed0b47EeE1F467D182620a333Fe69415Ba9AC9',
+};
+
+const POOLS = {
+    mainnet: {
+        'ETH-INCH': '0x438b5cec447d41cf070c7dec060bb2ecb260579c',
+    },
+    'mainnet-fork': {
+        'ETH-INCH': '0x438b5cec447d41cf070c7dec060bb2ecb260579c',
+    },
+};
+
+const REWARD_DISTRIBUTION = {
+    mainnet: '0x1cB37a0606003654b302bbD8fea408BFa066c6Ef',
+    'mainnet-fork': '0x1cB37a0606003654b302bbD8fea408BFa066c6Ef',
+};
+
+const REWARDS = {
+    mainnet: {
+        'ETH-INCH': ether('20000'),
+    },
+    'mainnet-fork': {
+        'ETH-INCH': ether('20000'),
+    },
+};
+
+module.exports = function (deployer, network) {
+    return deployer.then(async () => {
+        const token = await TokenMock.at(TOKEN[network]);
+
+        for (const [pool_name, pool_addr] of Object.entries(POOLS[network])) {
+            const pool_rewards = await deployer.deploy(FarmingRewards, pool_addr, token.address);
+            await pool_rewards.setRewardDistribution(REWARD_DISTRIBUTION[network]);
+            await token.transfer(pool_rewards.address, REWARDS[network][pool_name]);
+            await pool_rewards.notifyRewardAmount(REWARDS[network][pool_name]);
+        }
+    });
+};
