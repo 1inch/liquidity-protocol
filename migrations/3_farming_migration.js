38d17bcae94971aaec66cf607be60e34b488904c
Mikhail Melnik <by.zumzoom@gmail.com>
2020-12-18 14:59:30 +0300

    add pools creation to migration

--- migrations/3_farming_migration.js
+++ migrations/3_farming_migration.js
@@ -1,21 +1,30 @@
 const TokenMock = artifacts.require('./mocks/TokenMock.sol');
 const FarmingRewards = artifacts.require('./inch/farming/FarmingRewards.sol');
+const MooniswapFactory = artifacts.require('./MooniswapFactory.sol');
 
 const TOKEN = {
     kovan: '0x9F6A694123e5599a07f984eb8c0F3A475F553A03',
     'kovan-fork': '0x9F6A694123e5599a07f984eb8c0F3A475F553A03',
     mainnet: '0x28ed0b47EeE1F467D182620a333Fe69415Ba9AC9',
     'mainnet-fork': '0x28ed0b47EeE1F467D182620a333Fe69415Ba9AC9',
-    test: '0x1cB37a0606003654b302bbD8fea408BFa066c6Ef',
-    coverage: '0x1cB37a0606003654b302bbD8fea408BFa066c6Ef',
 };
 
 const POOLS = {
     mainnet: {
-        'ETH-INCH': '0x438b5cec447d41cf070c7dec060bb2ecb260579c',
+        'ETH-DAI': ['0x0000000000000000000000000000000000000000', '0x6B175474E89094C44Da98b954EedeAC495271d0F'],
+        'ETH-USDC': ['0x0000000000000000000000000000000000000000', '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'],
+        'ETH-USDT': ['0x0000000000000000000000000000000000000000', '0xdAC17F958D2ee523a2206206994597C13D831ec7'],
+        'ETH-WBTC': ['0x0000000000000000000000000000000000000000', '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'],
+        'ETH-1INCH': ['0x0000000000000000000000000000000000000000', TOKEN['mainnet']],
+        'DAI-1INCH': ['0x6B175474E89094C44Da98b954EedeAC495271d0F', TOKEN['mainnet']],
     },
     'mainnet-fork': {
-        'ETH-INCH': '0x438b5cec447d41cf070c7dec060bb2ecb260579c',
+        'ETH-DAI': ['0x0000000000000000000000000000000000000000', '0x6B175474E89094C44Da98b954EedeAC495271d0F'],
+        'ETH-USDC': ['0x0000000000000000000000000000000000000000', '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'],
+        'ETH-USDT': ['0x0000000000000000000000000000000000000000', '0xdAC17F958D2ee523a2206206994597C13D831ec7'],
+        'ETH-WBTC': ['0x0000000000000000000000000000000000000000', '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'],
+        'ETH-1INCH': ['0x0000000000000000000000000000000000000000', TOKEN['mainnet-fork']],
+        'DAI-1INCH': ['0x6B175474E89094C44Da98b954EedeAC495271d0F', TOKEN['mainnet-fork']],
     },
 };
 
@@ -41,12 +50,14 @@ module.exports = function (deployer, network) {
         }
 
         const token = await TokenMock.at(TOKEN[network]);
+        const factory = await MooniswapFactory.at(FACTORY[network]);
 
-        for (const [poolName, poolAddr] of Object.entries(POOLS[network])) {
-            const poolRewards = await deployer.deploy(FarmingRewards, poolAddr, token.address);
+        for (const [_, [token0, token1]] of Object.entries(POOLS[network])) {
+            const pool = await factory.pools(token0, token1);
+            const poolRewards = await deployer.deploy(FarmingRewards, pool.address, token.address);
             await poolRewards.setRewardDistribution(REWARD_DISTRIBUTION[network]);
-            await token.transfer(poolRewards.address, REWARDS[network][poolName]);
-            await poolRewards.notifyRewardAmount(REWARDS[network][poolName]);
+            // await token.transfer(poolRewards.address, REWARDS[network][poolName]);
+            // await poolRewards.notifyRewardAmount(REWARDS[network][poolName]);
         }
     });
 };
