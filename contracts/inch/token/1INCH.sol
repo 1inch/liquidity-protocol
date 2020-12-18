0d79e190fa2aa34a9f11d0db95f9a9e0ec74af5c
Mikhail Melnik <by.zumzoom@gmail.com>
2020-12-18 16:51:14 +0300

    simplify token deps

--- contracts/inch/token/1INCH.sol
+++ contracts/inch/token/1INCH.sol
@@ -2,12 +2,11 @@
 
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "./ERC20Permit.sol";
 
 
-contract OneInch is ERC20, ERC20Permit, ERC20Burnable {
+contract OneInch is ERC20Permit, ERC20Burnable {
     constructor(address[] memory recepients, uint256[] memory amounts) public ERC20("1INCH Token", "1INCH") EIP712("1INCH Token", "1") {
         require(recepients.length == amounts.length, "arrays length does not match");
         for (uint256 i = 0; i < recepients.length; i++) {
