2e47e766298730afa04085204ec0a5682cd7d5bf
Mikhail Melnik <by.zumzoom@gmail.com>
2020-12-18 16:00:32 +0300

    add token contract

---
+++ contracts/inch/token/1INCH.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ERC20Permit.sol";
+
+
+contract OneInch is ERC20, ERC20Permit {
+    constructor(address[] memory recepients, uint256[] memory amounts) public ERC20("1INCH Token", "1INCH") EIP712("1INCH Token", "1") {
+        require(recepients.length == amounts.length, "arrays length does not match");
+        for (uint256 i = 0; i < recepients.length; i++) {
+            _mint(recepients[i], amounts[i]);
+        }
+        require(totalSupply() == 1.5e9 ether, "incorrect distribution");
+    }
+}
