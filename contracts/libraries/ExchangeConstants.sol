a6ec355db8693d3f76423f8694e38dc336fd715f
Mikhail Melnik <by.zumzoom@gmail.com>
2020-12-22 12:24:07 +0300

    update constants

--- contracts/libraries/ExchangeConstants.sol
+++ contracts/libraries/ExchangeConstants.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.6.0;
 
 
 library ExchangeConstants {
-    uint256 internal constant _MIN_LEFTOVER_REFERRAL_SHARE = 20e18; // 20%
-    uint256 internal constant _MAX_LEFTOVER_REFERRAL_SHARE = 100e18; // 100%
-    uint256 internal constant _DEFAULT_LEFTOVER_REFERRAL_SHARE = 100e18; // 100%
+    uint256 internal constant _MIN_LEFTOVER_REFERRAL_SHARE = 0.5e18; // 50%
+    uint256 internal constant _MAX_LEFTOVER_REFERRAL_SHARE = 1e18; // 100%
+    uint256 internal constant _DEFAULT_LEFTOVER_REFERRAL_SHARE = 1e18; // 100%
 }
