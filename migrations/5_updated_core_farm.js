87c979682fafecaff36820aa907341c21a211900
Mikhail Melnik <by.zumzoom@gmail.com>
2021-02-05 19:19:48 +0300

    fix

--- migrations/5_updated_core_farm.js
+++ migrations/5_updated_core_farm.js
@@ -46,7 +46,7 @@ const FACTORY = {
 
 module.exports = function (deployer, network) {
     return deployer.then(async () => {
-        if (network === 'test' || network === 'coverage' || network === 'mainnet') {
+        if (network === 'test' || network === 'coverage') {
             // migrations are not required for testing
             return;
         }
